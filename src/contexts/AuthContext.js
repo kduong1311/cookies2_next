136303d2fd3460fd2f9e5c665248d53a1e70a199
Trần Khánh Dương <tranduong13112003@gmail.com>
2025-06-24 15:00:44 +0700

    test s

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -14,7 +14,7 @@ export function AuthProvider({children}) {
 
     const checkAuthStatus = async () => {
         try {
-            const res = await fetch("http://103.253.145.7:3000/api/users/me", {
+            const res = await fetch("https://103.253.145.7/api/users/me", {
                 method: "GET",
                 credentials: "include",
             });
@@ -39,7 +39,7 @@ export function AuthProvider({children}) {
 
     const logout = async () => {
         try {
-            await fetch("http://103.253.145.7:3000/api/users/logout", {
+            await fetch("https://103.253.145.7/api/users/logout", {
                 method: "POST",
                 credentials: "include",
             });
