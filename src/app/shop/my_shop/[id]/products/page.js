39edd23b070392b1a65eeb36587d3611c13d4c3d
Trần Khánh Dương <tranduong13112003@gmail.com>
2025-07-14 01:13:01 +0700

    a

--- src/app/shop/my_shop/[id]/products/page.js
+++ src/app/shop/my_shop/[id]/products/page.js
@@ -19,6 +19,23 @@ export default function ProductsPage() {
   const [showCreateModal, setShowCreateModal] = useState(false);
   const [categoriesData, setCategoriesData] = useState([]);
 
+    useEffect(() => {
+  const fetchCategories = async () => {
+    try {
+      const res = await fetch("http://103.253.145.7:3003/api/categories");
+      const result = await res.json();
+      if (result.status === "success") {
+        setCategoriesData(result.data); // Lưu vào state
+      }
+    } catch (error) {
+      console.error("Lỗi tải danh mục:", error);
+    }
+  };
+
+  fetchCategories();
+  }, []);
+
+
   // Fetch products from API
   useEffect(() => {
     const fetchProducts = async () => {
@@ -145,22 +162,6 @@ export default function ProductsPage() {
       </div>
     );
   }
-
-  useEffect(() => {
-  const fetchCategories = async () => {
-    try {
-      const res = await fetch("http://103.253.145.7:3003/api/categories");
-      const result = await res.json();
-      if (result.status === "success") {
-        setCategoriesData(result.data); // Lưu vào state
-      }
-    } catch (error) {
-      console.error("Lỗi tải danh mục:", error);
-    }
-  };
-
-  fetchCategories();
-  }, []);
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 p-6">
