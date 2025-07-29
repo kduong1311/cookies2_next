00c07e041be9fec3e38da0d4f3973093e4f9cf74
Trần Khánh Dương <tranduong13112003@gmail.com>
2025-07-29 20:37:03 +0700

    a

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,35 +1,31 @@
-
+// layout.js or layout.jsx
 import "../app/globals.css"
 import MainLayout from "./feed/MainLayout";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { ConfirmProvider } from "@/contexts/ConfirmContext";
 import { CartProvider } from "@/contexts/CartContext";
 import { Toaster } from "react-hot-toast";
-import { icons } from "lucide-react";
 
 export const metadata = {
   title: "Cookies",
   description: "Discover, Watch & Shop Delicious Recipes",
   icons: {
-    icon: "/favicon.ico"
-  }
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <Head>
-    <link rel="icon" href="/favicon.ico" />
-    </Head>
       <body>
-      <ConfirmProvider>
-        <CartProvider>
-          <AuthProvider>
-            <MainLayout>{children}</MainLayout>
-            <Toaster position="top-center" reverseOrder={false} />
-          </AuthProvider>
-        </CartProvider>
-      </ConfirmProvider>
+        <ConfirmProvider>
+          <CartProvider>
+            <AuthProvider>
+              <MainLayout>{children}</MainLayout>
+              <Toaster position="top-center" reverseOrder={false} />
+            </AuthProvider>
+          </CartProvider>
+        </ConfirmProvider>
       </body>
     </html>
   );
