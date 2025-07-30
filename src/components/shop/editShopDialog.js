4aab8e6254ce69c1a43be4714e21462f4829554e
Trần Khánh Dương <tranduong13112003@gmail.com>
2025-07-30 22:26:14 +0700

    b'

--- src/components/shop/editShopDialog.js
+++ src/components/shop/editShopDialog.js
@@ -157,7 +157,7 @@ const EditShopDialog = ({ isOpen, onClose, shopId }) => {
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent className="max-w-6xl max-h-[60vh] overflow-y-auto bg-gray-900 text-white border-gray-700 hide-scrollbar">
+      <DialogContent className="max-w-5x4 max-h-[80vh] overflow-y-auto bg-gray-900 text-white border-gray-700 hide-scrollbar">
         <DialogHeader>
           <DialogTitle className="text-2xl font-bold bg-gradient-to-r from-orange-400 to-red-500 bg-clip-text text-transparent">
             Edit Shop Information
